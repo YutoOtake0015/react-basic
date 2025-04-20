6bb437656c28b302094bf8b67e9b06ddc8e899dc
YutoOtake <yuto.programming@gmail.com>
2025-04-21 04:09:27 +0900

    feat: Sample1

--- src/components/Sample1.js
+++ src/components/Sample1.js
@@ -1,25 +1,28 @@
 import React, { useState } from "react";
 
-const Sample1 = (props) => {
-  const clickHandlar = () => {
-    console.log("clicked");
-  };
-
-  const [count, setCount] = useState(0);
+const Sample1 = () => {
+  const [products, setProducts] = useState({ name: "", price: "" });
 
   return (
     <>
-      <button
-        onClick={() => {
-          setCount((prevCont) => prevCont + 1);
-          setCount((a) => a + 1);
-        }}
-      >
-        click {count}
-      </button>
-
-      <h1>hello {props.name}</h1>
-      <h1>hello react 2</h1>
+      <form>
+        <input
+          type="text"
+          value={products.name}
+          onChange={(evt) =>
+            setProducts({ ...products, name: evt.target.value })
+          }
+        />
+        <input
+          type="text"
+          value={products.price}
+          onChange={(evt) =>
+            setProducts({ ...products, price: evt.target.value })
+          }
+        />
+      </form>
+      <h3>Product Name is {products.name}</h3>
+      <h3>Product Price is {products.price}</h3>
     </>
   );
 };
