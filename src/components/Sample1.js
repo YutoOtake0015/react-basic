786253c1e2e1eb09f379697e907b38b8f0d22a53
YutoOtake <yuto.programming@gmail.com>
2025-04-21 04:00:32 +0900

    feat: add useState sample

--- src/components/Sample1.js
+++ src/components/Sample1.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Sample1 = (props) => {
   const clickHandlar = () => {
     console.log("clicked");
   };
 
+  const [count, setCount] = useState(0);
+
   return (
     <>
-      <button onClick={clickHandlar}>click</button>
+      <button
+        onClick={() => {
+          setCount((prevCont) => prevCont + 1);
+          setCount((a) => a + 1);
+        }}
+      >
+        click {count}
+      </button>
+
       <h1>hello {props.name}</h1>
       <h1>hello react 2</h1>
     </>
