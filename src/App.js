109fbab1ed46d7404e0de6377813bde48c46abff
YutoOtake <yuto.programming@gmail.com>
2025-04-23 22:57:09 +0900

    add useContext practice

--- src/App.js
+++ src/App.js
@@ -4,18 +4,23 @@ import Sample2 from "./components/Sample2";
 import SampleUseEffect from "./components/SampleUseEffect";
 import TimerContainer from "./components/TimerContainer";
 import ApiFetch from "./components/ApiFetch";
+import AppContext from "./components/AppContext";
+import B from "./components/B";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* <Sample2 /> */}
-        {/* <SampleUseEffect /> */}
-        {/* <TimerContainer /> */}
-        <ApiFetch />
-      </header>
-    </div>
+    <AppContext.Provider value={"value from App.js"}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {/* <Sample2 /> */}
+          {/* <SampleUseEffect /> */}
+          {/* <TimerContainer /> */}
+          <ApiFetch />
+          <B />
+        </header>
+      </div>
+    </AppContext.Provider>
   );
 }
 
