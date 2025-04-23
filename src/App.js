649013b19743c0d8c65aa78e7808971a7c30a39e
YutoOtake <yuto.programming@gmail.com>
2025-04-23 23:24:17 +0900

    feat: Add useContext + useReducer practice

--- src/App.js
+++ src/App.js
@@ -4,20 +4,44 @@ import Sample2 from "./components/Sample2";
 import SampleUseEffect from "./components/SampleUseEffect";
 import TimerContainer from "./components/TimerContainer";
 import ApiFetch from "./components/ApiFetch";
-import AppContext from "./components/AppContext";
+import AppContext from "./context/AppContext";
 import B from "./components/B";
+import SampleReducer from "./components/SampleReducer";
+import CompB from "./components/CompB";
+import CompC from "./components/CompC";
+
+import React, { useReducer } from "react";
+
+const initialState = 0;
+const reducer = (currentState, action) => {
+  switch (action) {
+    case "add_1":
+      return currentState + 1;
+    case "multiple_3":
+      return currentState * 3;
+    case "reset":
+      return initialState;
+    default:
+      return currentState;
+  }
+};
 
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialState);
+
   return (
-    <AppContext.Provider value={"value from App.js"}>
+    <AppContext.Provider value={{ dispatchProvided: dispatch }}>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           {/* <Sample2 /> */}
           {/* <SampleUseEffect /> */}
           {/* <TimerContainer /> */}
-          <ApiFetch />
-          <B />
+          {/* <ApiFetch />
+          <B /> */}
+          {/* <SampleReducer /> */}
+          Count {count}
+          <CompB />
         </header>
       </div>
     </AppContext.Provider>
